6bad6192d8fb70760a78fcecb63691000ef83095
nimamot <64922998+nimamot@users.noreply.github.com>
2022-01-11 19:50:00 -0800

    minor changes

---
+++ src/com/mosh2022/Main.java
@@ -0,0 +1,17 @@
+package com.mosh2022;
+// terminate statements with a ';'
+import java.util.Date;
+
+public class Main {
+    // main should always be static
+    // we use primitive data type to store values,
+    // and reference data for storing complex objects
+    public static void main(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        // the new var is an instance of Date class: an object
+        System.out.println(now);
+
+
+    }
+}
